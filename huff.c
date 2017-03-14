cf5445700b483e38746d8bebf9f46bd8e0ed3249
RaulBritto <raul.britto08@gmail.com>
2017-03-14 02:43:27 -0300

    contador de frequencia

--- huff.c
+++ huff.c
@@ -7,55 +7,136 @@
 #define MAX 255
 
 
-typedef struct nodeHeap nodeHeap;
-struct nodeHeap
+
+void Usage(){
+	printf("Usage: ./ Input_File Output_file \n");
+	exit(1);
+}
+
+void printTable(unsigned int p[256])
 {
-	unsigned int frequency;
-	char code;
-};
+	int i = 0;
+	
+	for(i = 0; i < 256; i++)
+	{
+		if(p[i] == 0) continue;
+		printf("indice array: %d => code: (%c) freq: %d\n", i, (char) i , p[i]);
+	}
+}
+
+void PrintTable2(unsigned int  t[256][256]){
+	int i, j, cont = 0;
+
+	printf("Imprimindo tabela de contexto:\n");
+	for(i=0; i < 256; i++)
+		for(j=0; j < 256; j++){
+			if(t[i][j] == 0) continue;
+			cont++;
+			printf("Contexto [%c][%c] = %d\n",(char) i, (char) j, t[i][j] );
+		}
+	printf("Numero de contexto encontrados: %d\n", cont );
+}
+
+void swapChar(char c[2]){
+	char aux;
+
+	aux = c[0];
+	c[0] = c[1];
+	c[1] = aux;
+
+}
+
+void FrequencyCounterContext1(char *input_file){
+	FILE *file;
+	char c, first_character, buffer[2];
+	unsigned int frequency_table[256][256];
+	int i = 0, j = 0;
+
+	file = fopen(input_file, "r");
+	if(file == NULL){
+		printf("Error ao ler arquivo!\n");
+		exit(1);
+	}
+
+
+	for(i=0; i < 256; i++)
+		for(j=0; j < 256; j++)
+			frequency_table[i][j] = 0;	
+	
+	
+	fscanf(file,"%c",&first_character);	
+	//printf("Primeiro caracter: %c\n", first_character );
+	if(c == EOF){
+		printf("Nao foi possivel fazer a compactacao. Arquivo muito pequeno.\n");
+	}
+
+
+	buffer[0] = first_character;
+	//printf("buffer = [%c][ ]\n", buffer[0] );
+
+
+	while(!feof(file)){
+
+		fscanf(file,"%c", &c);	
+		buffer[1] = c;
+		//printf("buffer = [%c][%c]\n", buffer[0], buffer[1] );
+		frequency_table[ (int) buffer[0] ][(int) buffer[1]]++;
+		//printf("Contexto [%c][%c] = %d\n",  buffer[0], buffer[1] ,frequency_table[ (int) buffer[0] ][(int) buffer[1]] );
+		swapChar(buffer);
+	}	
+
+	PrintTable2(frequency_table);
+}
+
 
 
 void FrequencyCounter(char *input_file){
 
 	FILE *file;
 	char c;
-	nodeHeap table[256];
+	unsigned int frequency_table[256];
 	int i = 0;
 
 
-	file = fopen("input_file", "r");
+	file = fopen(input_file, "r");
 	if(file == NULL){
 		printf("Error ao ler arquivo!\n");
 		exit(1);
 	}
 
 	for(i=0; i < 255; i++){
-		table[i].frequency = 0;
-		table[i].code = (char) i;
+		frequency_table[i] = 0;
 	}
 
 	while(!feof(file)){
 		fscanf(file,"%c",&c);
-		printf("%c\n", c );
+		//printf("%c\n", c );
 		i = (int) c;
-		table[i].frequency++;
+		frequency_table[i]++;
 	}
+
+	printTable(frequency_table);
 
 	fclose(file);
 }
 
 void CompressFile(char *name_input, char *name_output){
 	
-	FrequencyCounter(name_input);
+	FrequencyCounterContext1(name_input);
 
 }
 
 
 
-int main(){
-    char name[60], name2[60];
-    printf("Digite o nome de arquivo de entrada e o de saida: ");
-    scanf("%s %s", name, name2);
+int main(int argc, char *argv[]){
+
+	char name[60], name2[60];
+
+	//if(argc != 4)		Usage();
+	
+    strcpy(name, argv[1]);
+    strcpy(name2, argv[2]);
+    
     CompressFile(name, name2);
     
     return 0;
